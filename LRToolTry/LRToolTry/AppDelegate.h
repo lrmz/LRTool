a871f824ccce9250ac05607fe150ef68771e259f
apple <apple@apples-MacBook-Pro.local>
2017-05-04 15:42:58 +0800

    begin

---
+++ LRToolTry/LRToolTry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRToolTry
+//
+//  Created by apple on 17/5/4.
+//  Copyright © 2017年 LR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRToolTry/LRToolTry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LRToolTry
+//
+//  Created by apple on 17/5/4.
+//  Copyright © 2017年 LR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
